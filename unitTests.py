10f01e04cf73284242eb706b8a7290b2c513ae63
amelll1 <amelisa800@gmail.com>
2024-04-18 17:44:17 -0500

    added the validate chart typr

--- unitTests.py
+++ unitTests.py
@@ -1,5 +1,5 @@
 import unittest
-from inputValidation import validate_symbol
+from inputValidation import validate_symbol, validate_chart_type
 
 class TestInputValidation(unittest.TestCase):
     def test_validate_symbol(self):
@@ -8,3 +8,10 @@ class TestInputValidation(unittest.TestCase):
         self.assertFalse(validate_symbol("aapl"))  
         self.assertFalse(validate_symbol("AAP"))   
         self.assertFalse(validate_symbol("AAPL123")) 
+
+    def test_validate_chart_type(self):
+        self.assertTrue(validate_chart_type('1'))  
+        self.assertTrue(validate_chart_type('2'))  
+        self.assertFalse(validate_chart_type('3'))  
+
+
